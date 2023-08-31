287211ba51f92204633973b134b0290811d5864f
Sergi Busquets <sergibusquetslobera@gmail.com>
2023-08-31 12:28:09 +0200

    Solved merged conflicts

--- blackjack/computer_vision/computer_vision/interface/main.py
+++ blackjack/computer_vision/computer_vision/interface/main.py
@@ -1,13 +1,5 @@
-<<<<<<< HEAD:blackjack/computer_vision/main.py
-from blackjack.computer_vision.model import load_roboflow_model, predict_roboflow_model
-from blackjack.computer_vision.clustering import (
-    cluster_one_player,
-    cluster_one_player_advanced,
-)
-=======
 from computer_vision.interface.model import load_roboflow_model, predict_roboflow_model
 from computer_vision.interface.clustering import cluster_one_player
->>>>>>> bd1bc4e26892c081653414ec7f3ad90c498b5f19:blackjack/computer_vision/computer_vision/interface/main.py
 
 
 if __name__ == "__main__":
