e19ec6e6951a04ca782804909357b2a608578025
Sergi Busquets <sergibusquetslobera@gmail.com>
2023-09-07 07:45:21 +0200

    API working on contours. Output coordinates to be checked

--- blackjack/inference/inference/api/fast.py
+++ blackjack/inference/inference/api/fast.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 import cv2
 
 from inference.interface.preprocessing import preprocess_image, find_contours
+from inference.interface.preprocessing import place_contours_on_bg, recreate_to_orig_rel
 from inference.interface.yolo import load_model, cards_prediction, class_mapping
 
 app = FastAPI()
@@ -29,6 +30,120 @@ def index():
 
 
 @app.post("/card_predictions")
+async def receive_image(img: UploadFile = File(...)):
+    # Load background
+    bg = cv2.imread(
+        "/Users/sergi/code/seeergiii/blackjack/blackjack/inference/inference/interface/bg.jpeg"
+    )  # TODO change path
+    bg = cv2.resize(bg, (1800, 1800))
+
+    # Save cached model
+    model = app.state.model
+
+    # Read image (aka video frame)
+    contents = await img.read()
+    nparr = np.fromstring(contents, np.uint8)
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)  # type(cv2_img) => numpy.ndarray
+
+    (
+        example,
+        x_orig,
+        y_orig,
+        w_orig,
+        h_orig,
+        x_offset,
+        y_offset,
+        contours,
+        image_resized_re,
+    ) = place_contours_on_bg(image, bg, scale_factor=2)
+
+    countour_list = []
+    countour_index = 0
+    detections = []
+
+    predicted_cards = []
+    clean_boxes = []
+    clean_confidences = []
+
+    for index, frame_cropped in enumerate(example):
+        frame_recoded = cv2.imencode(".jpg", frame_cropped)[1].tobytes()
+
+        decoded_img = tf.io.decode_image(
+            frame_recoded, channels=3, dtype=tf.dtypes.uint8
+        )
+        resized_img = tf.image.resize(decoded_img, (416, 416))
+        expanded_img = tf.expand_dims(resized_img, axis=0)
+        casted_img = tf.cast(expanded_img, tf.float32)
+        y_pred = model.predict(casted_img)
+
+        # Convert TF image to Cv2 Image
+        recoded = casted_img[0].numpy()
+        opencv_image = recoded.astype("uint8")
+
+        num_detections = y_pred["num_detections"][0]
+        detected_classes = y_pred["classes"][0][0:num_detections].tolist()
+
+        if num_detections > 0:
+            detections.append(detected_classes)
+
+            # Save predictions output to individual np.ndarrays
+            boxes_frame = y_pred["boxes"][0]
+            detections.append(y_pred["classes"][0][0:num_detections].tolist())
+            confidences_frame = y_pred["confidence"][0]
+
+            # Convert from np.ndarrays to regular lists
+            clean_boxes_frame = [box.tolist() for box in boxes_frame[:num_detections]]
+            clean_confidences_frame = confidences_frame[:num_detections].tolist()
+
+            # Convert clean_classes to real card codes
+            predicted_cards_frame = [class_mapping[card] for card in detected_classes]
+
+            # Append detections from the frame to results
+            predicted_cards.append(predicted_cards_frame)
+            clean_boxes.append(clean_boxes_frame)
+            clean_confidences.append(clean_confidences_frame)
+
+        else:
+            detections.append(None)
+            predicted_cards.append(None)
+            clean_boxes.append(None)
+            clean_confidences.append(None)
+
+        boxes_in_countour_list = []
+
+        for index in range(num_detections):
+            bbox = y_pred["boxes"][0][index]
+            countour = contours[countour_index]
+
+            x_recreated, y_recreated, width_r, height_r = recreate_to_orig_rel(
+                image,
+                example[countour_index],
+                image_resized_re,
+                countour,
+                bbox,
+                x_offset,
+                y_offset,
+            )
+
+            boxes_in_countour_list.append([x_recreated, y_recreated, width_r, height_r])
+
+        if boxes_in_countour_list:
+            countour_list.append(boxes_in_countour_list)
+        else:
+            countour_list.append(None)
+
+        countour_index += 1
+
+    return {
+        "total contours": len(example),
+        "contours shape": countour_list,
+        "cards detected": predicted_cards,
+        "detections's bounding boxes": clean_boxes,
+        "confidence": clean_confidences,
+    }
+
+
+@app.post("/card_predictions_cropped")
 async def receive_image(img: UploadFile = File(...)):
     """
     Given an image, returns predictions and clusters from its cropped countours.
--- blackjack/inference/inference/interface/preprocessing.py
+++ blackjack/inference/inference/interface/preprocessing.py
@@ -85,3 +85,128 @@ def find_contours(preprocessed_image: np.ndarray) -> list:
     print("✅ found and cropped contours in image")
 
     return {"images": images_cropped, "bounding_boxes": bounding_boxes}
+
+
+def place_contours_on_bg(
+    original_image: np.ndarray, bg_image: np.ndarray, scale_factor=1.5
+):
+    image_resized = imutils.resize(original_image, height=1000, width=1000)
+    image_greyscale = cv2.cvtColor(image_resized, cv2.COLOR_BGR2GRAY)
+    image_blurred = cv2.GaussianBlur(image_greyscale, (5, 5), 0)
+    image_canny_kernel = cv2.Canny(image_blurred, 50, 150)
+    contours, _ = cv2.findContours(
+        image_canny_kernel, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+    )
+
+    modified_bgs = []
+    clean_contours = []
+
+    preprocessed_image_size = image_canny_kernel.shape[0] * image_canny_kernel.shape[1]
+
+    for contour in contours:
+        mask = np.zeros_like(image_resized)
+        cv2.drawContours(mask, [contour], -1, (255, 255, 255), thickness=cv2.FILLED)
+        contour_content = cv2.bitwise_and(image_resized, mask)
+
+        # Create bounding box from contour to crop the alpha mask
+        x, y, w, h = cv2.boundingRect(contour)
+
+        if ((w * h) / preprocessed_image_size) > 0.01 and w / h > 0.25 and h / w > 0.25:
+            clean_contours.append(contour)
+            print(x, y, w, h)
+            alpha_mask = mask[y : y + h, x : x + w][:, :, 0]
+
+            # Crop only the exact shape of the contour
+            cropped_content = contour_content[y : y + h, x : x + w]
+
+            # Scale both the cropped_content and the alpha mask
+            scaled_content = cv2.resize(
+                cropped_content, None, fx=scale_factor, fy=scale_factor
+            )
+            scaled_alpha = cv2.resize(
+                alpha_mask, None, fx=scale_factor, fy=scale_factor
+            )
+
+            x_offset = (bg_image.shape[1] - scaled_content.shape[1]) // 2
+            y_offset = (bg_image.shape[0] - scaled_content.shape[0]) // 2
+
+            print(x_offset, y_offset)
+
+            temp_bg = bg_image.copy()
+
+            # Convert grayscale alpha mask to 3 channel
+            alpha_color = cv2.merge([scaled_alpha, scaled_alpha, scaled_alpha])
+
+            # Normalize the alpha mask
+            alpha_normalized = alpha_color / 255.0
+
+            # Place the scaled contour content
+            temp_bg[
+                y_offset : y_offset + scaled_content.shape[0],
+                x_offset : x_offset + scaled_content.shape[1],
+            ] = (
+                temp_bg[
+                    y_offset : y_offset + scaled_content.shape[0],
+                    x_offset : x_offset + scaled_content.shape[1],
+                ]
+                * (1 - alpha_normalized)
+                + scaled_content * alpha_normalized
+            )
+
+            modified_bgs.append(temp_bg)
+
+    print("✅ placed contours on background images")
+
+    return modified_bgs, x, y, w, h, x_offset, y_offset, clean_contours, image_resized
+
+
+def recreate_to_orig_rel(
+    image, last_image, resized_img, contour, bbox, x_offset, y_offset
+):
+    # Getting the coordinates of the cropped image inside of the resized image of Width 1000
+    x, y, w, h = cv2.boundingRect(contour)
+
+    # Convert to integers bbox - prediction of the model
+    center_x, center_y, w_pred, h_pred = map(int, bbox)
+
+    # Convert center-xywh to top-left x,y
+    top_left_x = center_x - (w_pred // 2)
+    top_left_y = center_y - (h_pred // 2)
+
+    print("top_left_x", top_left_x, top_left_y)
+    print("offset", x_offset / 1800, y_offset)
+    print((center_x - x_offset))
+    print((center_y - y_offset))
+
+    # 416 are the dimensions of the resized image for the model
+    x_rel, w_rel = top_left_x / 416, (top_left_x + w_pred) / 416
+    y_rel, h_rel = top_left_y / 416, (top_left_y + h_pred) / 416
+
+    # Getting shape of the cropped image
+    last_image_shape = last_image.shape
+
+    # Getting the relative coordinate of the card box inside of the cropped image
+    x_pr = int(x_rel * last_image_shape[1])
+    y_pr = int(y_rel * last_image_shape[0])
+    w_pr = int(w_rel * last_image_shape[1])
+    h_pr = int(h_rel * last_image_shape[1])
+
+    print(x_pr, y_pr, w_pr, h_pr)
+
+    print(resized_img.shape[1])
+
+    # Recreation of the coordinates on the rezised image to Width of 1000
+    x_100width = x / resized_img.shape[1]
+    w_100width = w / resized_img.shape[1]
+
+    y_100width = y / resized_img.shape[0]
+    h_100width = h / resized_img.shape[0]
+
+    # Recreation of the original cordinates
+    final_x = int(x_100width * image.shape[1])
+    final_w = int(w_100width * image.shape[1]) + final_x
+
+    final_y = int(y_100width * image.shape[0])
+    final_h = int(h_100width * image.shape[0]) + final_y
+
+    return final_x, final_y, final_w, final_h
